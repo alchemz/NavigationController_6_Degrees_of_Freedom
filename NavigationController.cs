25a701948f65d02e14f8dd32141ff9317bce2b67
Xianling(Lily) Zhang <alchemxz@gmail.com>
2017-11-16 14:06:47 -0500

    Update NavigationController.cs

--- NavigationController.cs
+++ NavigationController.cs
@@ -6,10 +6,15 @@ public class FlightController : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+    private Vector3 _rotation;
+    	
+	void Start()
+	{
+	    rotation = this.transform.rotation.eulerAngles;	
+	}
 
 	void Update ()
 	{
-	    var rotation = this.transform.rotation.eulerAngles;
 	    rotation += new Vector3(
             Input.GetAxis("Pitch") * rotationSpeed * Time.deltaTime,
             Input.GetAxis("Yaw") * rotationSpeed * Time.deltaTime,
@@ -19,4 +24,5 @@ public class FlightController : MonoBehaviour
 	    this.transform.position += this.transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime;
 	    this.transform.position += this.transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
     }
+    
 }
